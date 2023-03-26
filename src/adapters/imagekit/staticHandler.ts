3a6d6f7c6f949dd8122794ddec5a5dace502c08a
Alessio Gravili <alessio@gravili.de>
2023-03-26 15:37:54 +0200

    Add missing headers: content-type and content-length
    This also fixes support for displaying SVG images

--- src/adapters/imagekit/staticHandler.ts
+++ src/adapters/imagekit/staticHandler.ts
@@ -17,10 +17,21 @@ export const getHandler = ({
   folder,
 }: Args): StaticHandler => {
   return async (req, res, next) => {
+    const handleResponse = mapRes => {
+      // Forward the Content-Type header
+      res.setHeader('Content-Type', mapRes.headers['content-type'])
+
+      // Forward the Content-Length header. Note: if the image is modified in the process, this would need to change.
+      res.setHeader('Content-Length', mapRes.headers['content-length'])
+
+      // Pipe the response
+      mapRes.pipe(res)
+    }
+
     if (folder) {
-      https.get(`${urlEndpoint + folder}/${req.params.filename}`, mapRes => mapRes.pipe(res))
+      https.get(`${urlEndpoint + folder}/${req.params.filename}`, handleResponse)
     } else {
-      https.get(urlEndpoint + req.params.filename, mapRes => mapRes.pipe(res))
+      https.get(urlEndpoint + req.params.filename, handleResponse)
     }
 
     return res
