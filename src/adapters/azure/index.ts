64a3643e345f629dc25512153c87cb32774eb13f
Angelo Bartolome <angelo@activetheory.net>
2023-04-28 09:51:20 -0300

    azure-adapter: Add types, improve comments, improve existing types

--- src/adapters/azure/index.ts
+++ src/adapters/azure/index.ts
@@ -7,13 +7,44 @@ import { getHandleDelete } from './handleDelete'
 import { getHandleUpload } from './handleUpload'
 import { extendWebpackConfig } from './webpack'
 
-export interface Args {
-  connectionString?: string
+export type Args = {
+  /**
+   * The name of the container to use
+   * @see https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blobs-introduction#containers
+   * @example
+   * ```ts
+   * const containerName = 'my-container'
+   * ```
+   */
   containerName: string
   baseURL: string
+  /**
+   * Allow the adapter to create the container if it doesn't exist
+   */
   allowContainerCreate: boolean
-  storageClient?: ContainerClient
-}
+} & (
+  | {
+      /**
+       * Connection string for the Azure Storage Account
+       * @see https://learn.microsoft.com/en-us/azure/storage/common/storage-configure-connection-string
+       */
+      connectionString: string
+      storageClient?: never
+    }
+  | {
+      /**
+       * A custom Azure Storage Client
+       * @see https://docs.microsoft.com/en-us/javascript/api/@azure/storage-blob/containerclient?view=azure-node-latest
+       * @example
+       * ```ts
+       * import { ContainerClient } from '@azure/storage-blob'
+       * const containerClient = new ContainerClient(connectionString, containerName)
+       * ```
+       */
+      storageClient: ContainerClient
+      connectionString?: never
+    }
+)
 
 export const azureBlobStorageAdapter = ({
   connectionString,
@@ -28,13 +59,15 @@ export const azureBlobStorageAdapter = ({
     throw new Error('You must provide a connection string or a storage client')
   }
 
-  const getStorageClient = () => {
+  const getStorageClient: () => ContainerClient = () => {
     if (storageClient) return storageClient
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString!) // We know this is defined if storageClient is not
-    return (storageClient = blobServiceClient.getContainerClient(containerName))
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString as string)
+    storageClient = blobServiceClient.getContainerClient(containerName as string)
+    return storageClient
   }
 
-  const createContainerIfNotExists = () => {
+  const createContainerIfNotExists: () => void = () => {
     getStorageClient().createIfNotExists({ access: 'blob' })
   }
 
